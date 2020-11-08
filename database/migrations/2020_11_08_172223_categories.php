f5ceff7971cad8ef1de0c7c085d49843a46f2114
xiezehao <798351803@qq.com>
2020-11-08 17:34:23 +0800

    分类模型和数据
    分类模型和数据

--- database/migrations/2020_11_08_172223_categories.php
+++ database/migrations/2020_11_08_172223_categories.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class Categories extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->increments("id");
-            $table->string("name")->index()->comment("名称");
+        Schema::table('tablename', function (Blueprint $table) {
+            //
             $table->text("description")->nullable()->comment("描述");
             $table->integer("post_count")->default(0)->comment("帖子数");
-//            $table->timestamps();
         });
     }
 
@@ -29,6 +27,8 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::table('tablename', function (Blueprint $table) {
+            //
+        });
     }
 }
