a30399da3a609834526edc6fa456a2b3e9e517c3
xiezehao <798351803@qq.com>
2020-11-08 22:07:24 +0800

    分类话题列表
    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -2,3 +2,7 @@
 function route_class(){
     return str_replace(".","-",\Illuminate\Support\Facades\Route::currentRouteName());
 }
+
+function category_nav_active($category_id){
+    return active_class((if_route("categories.show") && if_route_param("category",$category_id)));
+}
