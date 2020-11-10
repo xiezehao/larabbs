5263317a0e90cc9c9424e9a6635a50567e963f6b
xiezehao <798351803@qq.com>
2020-11-10 16:59:25 +0800

    消息通知有新回复
    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,9 +9,23 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
     public function creating(Reply $reply)
     {
-        //
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
